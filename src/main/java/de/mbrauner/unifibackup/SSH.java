136a639815cc3e9b560ea4e70de37976f72f7b1b
Max Brauner <mbrauner@acardo.com>
2020-11-27 08:32:07 +0100

    P: temporary files cannot be moved on synology
    S: direct write to final file

--- src/main/java/de/mbrauner/unifibackup/SSH.java
+++ src/main/java/de/mbrauner/unifibackup/SSH.java
@@ -23,74 +23,67 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class SSH {
 
-    private UnifiProperties p;
+  private UnifiProperties p;
 
-    public void perform() throws IOException, JSchException {
-        JSch jsch = new JSch();
-        String host = p.getProperty(UnifiProperties.HOST);
-        int port = UnifiProperties.PORT_DEFAULT;
+  public void perform() throws IOException, JSchException {
+    JSch jsch = new JSch();
+    String host = p.getProperty(UnifiProperties.HOST);
+    int port = UnifiProperties.PORT_DEFAULT;
 
-        if (p.getProperty(UnifiProperties.PORT, "").matches("\\d+")) {
-            port = Integer.parseInt(p.getProperty(UnifiProperties.PORT));
-        }
-
-        Session session = jsch.getSession(p.getProperty(UnifiProperties.USER), host, port);
-        session.setPassword(p.getProperty(UnifiProperties.PW));
-        session.setConfig("StrictHostKeyChecking", "no");
-        session.connect();
-
-        List<String> fileNames = listFiles(session);
-        getFiles(session, fileNames);
-
-        session.disconnect();
+    if (p.getProperty(UnifiProperties.PORT, "").matches("\\d+")) {
+      port = Integer.parseInt(p.getProperty(UnifiProperties.PORT));
     }
 
-    protected void getFiles(Session session, List<String> fileNames) throws IOException, JSchException {
-        Channel channel = session.openChannel("exec");
-        for (String fileName : fileNames) {
-            LOGGER.debug("try to fetch  {}", fileName);
-            ChannelExec.class.cast(channel).setCommand("scp -f " + fileName);
-            InputStream commandOutput = channel.getInputStream();
-            channel.connect();
-            byte[] arr = new byte[commandOutput.available()];
-            int stillAvailable = commandOutput.read(arr);
-            if (stillAvailable > 0) {
-                LOGGER.warn("possible error detected, still {} bytes to read", stillAvailable);
-            }
-            channel.disconnect();
-            File tmp = File.createTempFile("unifibackup", ".tmp");
-            try (FileOutputStream fos = new FileOutputStream(tmp);) {
-                fos.write(arr);
-                fos.flush();
-                final String dest = getRightPath(p.getProperty(UnifiProperties.LOCALPATH, UnifiProperties.LOCALPATH_DEFAULT))
-                    + fileName.substring(fileName.lastIndexOf(File.separator) + 1);
-                if (tmp.renameTo(new File(dest))) {
-                    LOGGER.debug("moved tmp file to {}", dest);
-                    tmp.deleteOnExit();
-                } else {
-                    LOGGER.error("cannot move tmp file!");
-                }
-            }
-            LOGGER.info("downloaded {}", fileName);
-        }
-    }
+    Session session = jsch.getSession(p.getProperty(UnifiProperties.USER), host, port);
+    session.setPassword(p.getProperty(UnifiProperties.PW));
+    session.setConfig("StrictHostKeyChecking", "no");
+    session.connect();
 
-    private String getRightPath(@NonNull String path) {
-        if (!path.endsWith(File.separator)) {
-            return path + File.separator;
-        } else {
-            return path;
-        }
-    }
+    List<String> fileNames = listFiles(session);
+    getFiles(session, fileNames);
 
-    protected List<String> listFiles(Session session) throws IOException, JSchException {
-        Channel channel = session.openChannel("exec");
-        ChannelExec.class.cast(channel).setCommand("ls " + p.getProperty(UnifiProperties.REMOTE, UnifiProperties.REMOTE_DEFAULT));
-        InputStream commandOutput = channel.getInputStream();
-        channel.connect();
-        List<String> list = IOUtils.readLines(commandOutput, Charset.defaultCharset());
-        channel.disconnect();
-        LOGGER.debug("listing return {}", list);
-        return list;
+    session.disconnect();
+  }
+
+  protected void getFiles(Session session, List<String> fileNames) throws IOException, JSchException {
+    Channel channel = session.openChannel("exec");
+    for (String fileName : fileNames) {
+      LOGGER.debug("try to fetch  {}", fileName);
+      ChannelExec.class.cast(channel).setCommand("scp -f " + fileName);
+      InputStream commandOutput = channel.getInputStream();
+      channel.connect();
+      byte[] arr = new byte[commandOutput.available()];
+      int stillAvailable = commandOutput.read(arr);
+      if (stillAvailable > 0) {
+        LOGGER.warn("possible error detected, still {} bytes to read", stillAvailable);
+      }
+      channel.disconnect();
+      final String file = getRightPath(p.getProperty(UnifiProperties.LOCALPATH, UnifiProperties.LOCALPATH_DEFAULT))
+        + fileName.substring(fileName.lastIndexOf(File.separator) + 1);
+      try (FileOutputStream fos = new FileOutputStream(file);) {
+        fos.write(arr);
+        fos.flush();
+      }
+      LOGGER.info("downloaded {}", fileName);
     }
+  }
+
+  private String getRightPath(@NonNull String path) {
+    if (!path.endsWith(File.separator)) {
+      return path + File.separator;
+    } else {
+      return path;
+    }
+  }
+
+  protected List<String> listFiles(Session session) throws IOException, JSchException {
+    Channel channel = session.openChannel("exec");
+    ChannelExec.class.cast(channel).setCommand("ls " + p.getProperty(UnifiProperties.REMOTE, UnifiProperties.REMOTE_DEFAULT));
+    InputStream commandOutput = channel.getInputStream();
+    channel.connect();
+    List<String> list = IOUtils.readLines(commandOutput, Charset.defaultCharset());
+    channel.disconnect();
+    LOGGER.debug("listing return {}", list);
+    return list;
+  }
 }
